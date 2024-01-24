799a7ca7f070ba0db21a9f8c9c053797e5666106
lauout123 <605933181@qq.com>
2024-01-24 21:53:14 +0800

    搭建项目并初始化

---
+++ src/router/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { RouteObject } from 'react-router-dom'
+import Discover from '@/views/discover'
+const routes: RouteObject[] = [
+  {
+    path: '/discover',
+    element: <Discover />,
+  },
+]
+
+export default routes
